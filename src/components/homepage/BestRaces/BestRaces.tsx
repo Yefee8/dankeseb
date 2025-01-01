f792665531b92b98e52928853817ace6db139cd4
yefee8 <yavuzefefindikli8@gmail.com>
2025-01-01 22:29:57 +0300

    -

--- src/components/homepage/BestRaces/BestRaces.tsx
+++ src/components/homepage/BestRaces/BestRaces.tsx
@@ -14,6 +14,24 @@ export default function BestRaces() {
       description: `The 2009 Chinese Grand Prix was Sebastian Vettel's second victory in Formula 1 and his first win with Red Bull Racing(It's also the first win for Red Bull Racing). Starting from pole position, Vettel showcased exceptional skill in challenging wet conditions. Despite the relentless rain and poor visibility, he maintained control throughout the race and crossed the finish line as the winner.`,
       image: "/images/races/2009-Chinese-Grand-Prix.jpg",
     },
+    {
+      id: "2010-abu-dhabi-grand-prix",
+      title: "2010 Abu Dhabi Grand Prix",
+      description: `The 2010 Abu Dhabi Grand Prix was the final and title-deciding race of an incredible 2010 season. Despite being considered an unlikely championship contender, Sebastian Vettel secured pole position in qualifying and went on to win the race. However, winning the race alone wasn't enough to guarantee the championship. If Fernando Alonso finished 4th, he would claim the title (if Alonso finished 5th, the points would be tied, but Vettel would still win on countback). In the end, Fernando Alonso got stuck in 6th place and couldn't improve his position. With that result, Sebastian Vettel clinched his first World Championship at the 2010 Abu Dhabi Grand Prix. <br/> "Okay Sebastian, good job. I need to wait until everybody crosses the line. I need to wait, but it's looking good. You just wait sunshine, you just wait." <br/> "DU BIST WELTMEISTER!"`,
+      image: "/images/races/2010-Abu-Dhabi-Grand-Prix.avif",
+    },
+    {
+      id: "2011-monza-grand-prix",
+      title: "2011 Monza Grand Prix",
+      description: `The 2011 Monza Grand Prix was expected to be a challenging race for Red Bull Racing. While the RB7 was an incredible car, it struggled on low-downforce tracks like Monza. Despite this, Sebastian Vettel secured pole position in qualifying. Although he briefly lost his position at the start, he managed to overtake Fernando Alonso and went on to claim victory.`,
+      image: "/images/races/2011-Monza-Grand-Prix.jpg",
+    },
+    {
+      id: "2011-spanish-grand-prix",
+      title: "2011 Spanish Grand Prix",
+      description: `The 2011 Spanish Grand Prix was a classic Red Bull-Vettel victory. Starting from pole position, Sebastian Vettel quickly pulled out of DRS range in the opening laps, dominating the race from start to finish. Even his team couldn’t slow him down in the final laps, as he set the last two fastest laps of the race himself, sealing yet another impressive victory.`,
+      image: "/images/races/2011-Spanish-Grand-Prix.jpg",
+    },
   ];
 
   return (
