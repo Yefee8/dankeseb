9a3226b4612a8276b28a916cfaaecbd5025fc1e4
yefee8 <yavuzefefindikli8@gmail.com>
2025-01-01 12:07:15 +0300

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import ChampionshipStats from "@/components/homepage/ChampionshipStats";
 import GeneralStats from "@/components/homepage/GeneralStats";
 import BmwSauber from "@/components/homepage/Teams/BmwSauber";
 import ToroRosso from "@/components/homepage/Teams/ToroRosso";
-import RedBullRacing from "@/components/homepage/Teams/RedBullRacing";
 import ScuderiaFerrari from "@/components/homepage/Teams/ScuderiaFerrari";
 import AstonMartin from "@/components/homepage/Teams/AstonMartin";
+import RedBullRacing from "@/components/homepage/Teams/RedBullRacing";
 
 export default function Home() {
   return (
