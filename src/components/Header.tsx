857137d5bc0529606d6c0d2f5f8df575533bbd83
yefee8 <yavuzefefindikli8@gmail.com>
2025-01-02 17:54:17 +0300

    -

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="absolute top-0 left-0 w-full py-2 flex justify-center md:justify-between gap-4 flex-wrap items-center md:px-16 px-8 z-50 duration-250">
       <a href="/">
-      <h1 className="text-2xl font-bold cursor-pointer">Danke Seb</h1>
+        <h1 className="text-2xl font-bold cursor-pointer">Danke Seb</h1>
       </a>
 
       <div className="flex flex-wrap justify-center gap-2 items-center font-medium">
@@ -35,6 +35,12 @@ export default function Header() {
           className="text-base px-2 py-1 hover:bg-yellow-500/70 rounded-lg duration-150"
         >
           Gallery
+        </a>
+        <a
+          href="https://github.com/Yefee8/dankeseb"
+          className="text-base px-2 py-1 hover:bg-indigo-500/70 rounded-lg duration-150"
+        >
+          Github
         </a>
       </div>
     </div>
